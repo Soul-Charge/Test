30346c10c8cd9d49fe9ddb6d01204e6cb1594e30
Soul-Charge <xiaoyaochn@outlook.com>
2019-08-19 16:49:24 +0800

    change test

--- test.c
+++ test.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(void)
+int main(int argc, char * argv[])
 {
+    FILE * my_file;
+    int descriptor;
     int i;
-    pid_t pid;
 
-    puts("I am father");
-    
-    for (i = 0; i < 10; i++)
+    if ((my_file = fopen("test.txt", "w")))
     {
-        pid = fork();
-        if (!pid)
-        {
-            printf("I am child %d\n", i + 1);
-            break;
-        }
+        fprintf(stderr, "Can't open test.txt\n");
+        exit(1);
     }
+    descriptor = fileno(my_file);
+
+    if (argc > 1)
+        dup2(descriptor, 1); //将标准输出重定向至my_file指向的文件
+
+    printf("--- print some number ---\n");
+    printf("*** run with any arguments will output to file:\"test.txt\" ***\n");
+    for (i = 0; i < 10, i++)
+    printf("  %d", i);
+
     return 0;
 }
