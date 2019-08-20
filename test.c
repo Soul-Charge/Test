c069541a00216d91248a66d0b407a0022e212d66
Soul-Charge <xiaoyaochn@outlook.com>
2019-08-20 14:31:10 +0800

    add check code in test.c

--- test.c
+++ test.c
@@ -16,9 +16,17 @@ int main(void)
     pid = fork();
     if (!pid)
     {
-        dup2(fileno(fp), 1); // redirect stdout to test.txt
+        fprintf(fp, "child: can I write the file?\n"); //check the child whether have the stream of test.txt
+        if (-1 == dup2(fileno(fp), 1)) // redirect stdout to test.txt
+        {
+            fprintf(stderr, "can't redirect stdout.\n");
+        }
+        else
+            puts("redirect stdout success.");
+
         execl("~/Test/test/out", "~/Test/test/out", NULL);
     }
+    fclose(fp);
 
     return 0;
 }
