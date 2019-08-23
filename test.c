42d0074b1ac4942b00fa767c04038199e2395395
Soul-Charge <xiaoyaochn@outlook.com>
2019-08-23 10:01:46 +0800

    change test

--- test.c
+++ test.c
@@ -1,49 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <errno.h>
+#include <signal.h>
+
+int catch_signal(int sig, void (*handler)(int));
 
 int main(void)
 {
-    pid_t pid;
-    FILE * fp;
-
-    if (NULL == (fp = fopen("test.txt", "w")))
-    {
-        fprintf(stderr, "Can't open test.txt\n");
-        exit(1);
-    }
-
-    pid = fork();
-    if (!pid)
-    {
-        puts("I am child");
-        fprintf(fp, "child: can I write the file?\n"); //check the child whether have the stream of test.txt
-        fprintf(fp, "Now I will redirect the stdout.\n");
-
-        if (-1 == dup2(fileno(fp), 1)) // redirect stdout to test.txt
-        {
-            fprintf(stderr, "can't redirect stdout.\n");
-        }
-        else
-        {
-            puts("redirect stdout success.");
-            puts("Now I will execute the out");
-        }
-
-        if (-1 == execl("./out", "./out", NULL))
-            fprintf(stderr, "can't execute out: %s", strerror(errno));
-        else //disable
-            puts("I had executed the out!");
-    }
-    else
-    {
-        //sleep(5); // father sleep 
-        puts("father end");
-    }
-
-    fclose(fp);
-
+    catch_signal(SIGINT, SIG_IGN);
+    system("pause");
     return 0;
+}
+
+/* 将函数设置为给定信号的处理器 */
+/* 调用方式: catch_signal(要处理的信号, 用来处理的函数名); */
+int catch_signal(int sig, void (*handler)(int))
+{
+    struct sigaction action;
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    return sigaction(sig, &action, NULL);
 }
